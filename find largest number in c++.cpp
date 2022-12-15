fbc6b030c2349a2a1fe8af05aee35700a7ca000f
213218 <120670807+213218@users.noreply.github.com>
2022-12-15 21:58:13 +0500

    Add files via upload

---
+++ find largest number in c++.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<" Enter first number"<<endl;
+	cin>>a;
+	cout<<" Enter Second number"<<endl;
+	cin>>b;
+	if(a>b)
+	{
+		cout<<"largest number is"<<" "<<a<<endl;
+	}
+	else
+	{
+				cout<<"largest number is"<<" "<<b<<endl;
+
+	}
+	
+}
